031761ae317405e45c3cf2bcea287ecaf9afc5e9
Dipsana Roy <dipsanaroy@gmail.com>
2025-07-22 14:29:42 +0000

    ✅ CE002 notes and example programs added
    This mini-guide covers three useful C standard library functions for handling strings and converting them to numbers: `strchr()`, `strtol()`, and `strtod()`.

---
+++ CE002_Srings/ce002_01_strchr.c
@@ -0,0 +1,19 @@
+// CE2.1. Show only strings which contains no more than two 'a'
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char *lines[] = {
+        "Hello! I am a String.",
+        "String says everyone to keep their curiosity alive.",
+        "Amazingly, banana paradise awaits Alaba at Alabama."
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (strchr(strchr(strchr(lines[i], 'a') + 1, 'a') + 1, 'a') == NULL)
+            puts(lines[i]);
+    }
+    return 0;
+}
